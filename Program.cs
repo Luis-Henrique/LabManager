bad61185f69f977c6a5fa3d76613678433cab8fe
Luis Henrique <henriqueluis3104@hotmail.com>
2022-05-26 12:38:30 -0300

    add model and repository computer

--- Program.cs
+++ Program.cs
@@ -1,90 +1,86 @@
 ﻿using Microsoft.Data.Sqlite;
 using LabManager.Database;
+using LabManager.Repositories;
 
 var databaseSetup = new DatabaseSetup();
 
 var modelName = args[0];
 var modelAction = args[1];
 
-if (modelName == "Computer")
+if(modelName == "Computer")
 {
-    if (modelAction == "List")
+    var computerRepository = new ComputerRepository();
+
+    if(modelAction == "List")
     {
-        var connection = new SqliteConnection("Data Source=database.db");
-        connection.Open();
-        var command = connection.CreateCommand();
-
-        command.CommandText = $"SELECT * FROM Computers;";
-
-        var reader = command.ExecuteReader();
-        while (reader.Read())
+        Console.WriteLine("Computer List");
+       
+        foreach (var computer in computerRepository.GetAll())
         {
-            Console.WriteLine($"{reader.GetInt32(0)} {reader.GetString(1)} {reader.GetString(2)}");
+            Console.WriteLine($"{computer.Id}, {computer.Ram}, {computer.Processor}");
         }
-        connection.Close();
     }
-    else if (modelAction == "New")
+
+    if(modelAction == "New")
     {
         var id = Convert.ToInt32(args[2]);
-        var ram = args[3];
-        var processor = args[4];
-        
+        string ram = args[3];
+        string processor = args[4];
+
         var connection = new SqliteConnection("Data Source=database.db");
         connection.Open();
+
         var command = connection.CreateCommand();
-
-        command.CommandText = $"INSERT INTO Computers VALUES($id, $ram, $processor)";
-
+        command.CommandText = "INSERT INTO Computers VALUES($id, $ram, $processor)";
         command.Parameters.AddWithValue("$id", id);
         command.Parameters.AddWithValue("$ram", ram);
         command.Parameters.AddWithValue("$processor", processor);
         command.ExecuteNonQuery();
+
         connection.Close();
     }
 }
-else if (modelName == "Lab")
+if(modelName == "Lab")
 {
-    switch (modelAction)
+    if(modelAction == "List")
     {
-        case "New":
-            {
-                var id = Convert.ToInt32(args[2]);
-                var number = args[3];
-                var name = args[4];
-                var block = args[5];
+        Console.WriteLine("Lab List");
 
-                var connection = new SqliteConnection("Data Source=database.db");
-                connection.Open();
-                var command = connection.CreateCommand();
+        var connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
 
-                command.CommandText = $"INSERT INTO Labs VALUES($id, $number, $name, $block)";
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Lab";
+        
+        var reader = command.ExecuteReader();
+        
+        while(reader.Read())
+        {
+            Console.WriteLine("{0}, {1}, {2}, {3}", reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2), reader.GetString(3));
+        }
+        
+        connection.Close();
+    }
+    if(modelAction == "New")
+    {
+        var id = Convert.ToInt32(args[2]);
+        var number = args[3];
+        string name = args[4];
+        string block = args[5];
 
-                command.Parameters.AddWithValue("$id", id);
-                command.Parameters.AddWithValue("$number", number);
-                command.Parameters.AddWithValue("$name", name);
-                command.Parameters.AddWithValue("$block", block);
-                command.ExecuteNonQuery();
-                connection.Close();
-                break;
-            }
-        case "List":
-            {
-                var connection = new SqliteConnection("Data Source=database.db");
-                connection.Open();
-                var command = connection.CreateCommand();
+        var connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+        
+        var command = connection.CreateCommand();
+        
+        command.CommandText = "INSERT INTO Lab VALUES($id, $number, $name, $block)";
+        command.Parameters.AddWithValue("$id", id);
+        command.Parameters.AddWithValue("$number", number);
+        command.Parameters.AddWithValue("$name", name);
+        command.Parameters.AddWithValue("$block", block);
 
-                command.CommandText = $"SELECT * FROM Labs;";
+        command.ExecuteNonQuery();
 
-                var reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    Console.WriteLine($"{reader.GetInt32(0)} {reader.GetString(1)} {reader.GetString(2)} {reader.GetString(3)}");
-                }
-                connection.Close();
-                break;
-            }
-        default:
-            Console.WriteLine("Comando inválido");
-            break;
+        connection.Close();
     }
 }
